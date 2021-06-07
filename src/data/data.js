da9337f10014a35ec34fbabe655a89933bffc401
randy chong <randychong27@yahoo.com>
2021-06-07 09:38:10 -0500

    created data for products

---
+++ src/data/data.js
@@ -0,0 +1,77 @@
+import affogato from "../images/affogato";
+import americano from "../images/americano";
+import cappucino from "../images/cappucino";
+import espresso from "../images/espresso";
+import honey from "../images/honey-green-tea";
+import lemon from "../images/lemon-wintermelon-tea";
+import matcha from "../images/matcha-green-tea";
+import lychee from "../images/lychee-oolong-tea";
+import thai from "../images/thai-tea";
+import viet from "../images/viet-coffee";
+
+export const data = [
+  {
+    products: [
+      {
+        id: 1,
+        name: "Espresso",
+        price: 3.0,
+        img: { espresso },
+      },
+      {
+        id: 2,
+        name: "Americano",
+        price: 3.75,
+        img: { americano },
+      },
+      {
+        id: 3,
+        name: "Cappucino",
+        price: 4.0,
+        img: { cappucino },
+      },
+      {
+        id: 4,
+        name: "Vietnamese Coffee",
+        price: 4.75,
+        img: { viet },
+      },
+      {
+        id: 5,
+        name: "Affogato",
+        price: 6.0,
+        img: { affogato },
+      },
+      {
+        id: 6,
+        name: "Matcha Green Tea",
+        price: 4.0,
+        img: { matcha },
+      },
+      {
+        id: 7,
+        name: "Honey Green Tea",
+        price: 4.25,
+        img: { honey },
+      },
+      {
+        id: 8,
+        name: "Lychee Oolong Tea",
+        price: 4.25,
+        img: { lychee },
+      },
+      {
+        id: 9,
+        name: "Lemon Wintermelon Tea",
+        price: 4.5,
+        img: { lemon },
+      },
+      {
+        id: 10,
+        name: "Thai Tea",
+        price: 4.5,
+        img: { thai },
+      },
+    ],
+  },
+];
