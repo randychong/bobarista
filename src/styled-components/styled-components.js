82e0a231ef564818d7c3f8f902cd0e69dbd6a206
randy chong <randychong27@yahoo.com>
2021-06-07 21:24:17 -0500

    reduce border radius for buttons and images

--- src/styled-components/styled-components.js
+++ src/styled-components/styled-components.js
@@ -4,11 +4,16 @@ export const Button = styled.button`
   height: 2rem;
   width: 10rem;
   margin-bottom: 1rem;
+  font-family: "Raleway", sans-serif;
+  font-size: 16px;
+  border-radius: 3px;
+  border: 1px black solid;
 `;
 
 export const ProductImage = styled.img`
-  height: 200px;
-  width: 200px;
+  height: 250px;
+  width: 250px;
+  border-radius: 3px;
 `;
 
 export const AppHeader = styled.h1`
